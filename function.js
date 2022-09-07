b6e045fc10cd6088d9ba7f5d02564b6df67dc1c8
LeeJinKyum <109436439+LeeJinKyum@users.noreply.github.com>
2022-09-08 00:09:46 +0900

    Login Function

--- function.js
+++ function.js
@@ -1,5 +1,17 @@
 //ID, PW 둘 중에 하나라도 빈칸이면, sign in 버튼 클릭 시
 //안내 경고문이 뜨게 만들기
+function login() {
+    const id = document.querySelector('#user_id');
+    const pw = document.querySelector('#user_pw');
+
+    if(id.value == "" || pw.value == ""){
+        alert("다시 입력하세요.");
+    }else{
+        location.href = 'main.html';
+    }
+}
+
+// 현재 시각 나타내는 기능
 const clock = document.querySelector("div#clock");
 
 function getClock() {
